089579d96f58f10277996164044a5aef3a42868a
spd12345 <64862751+spd12345@users.noreply.github.com>
2023-11-10 00:18:21 +0530

    aT3

---
+++ AngularTask/AngularTask3/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { RequestInfo } from 'undici-types';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularTask3';
+  constructor(){
+    this.fetchData(this.api)
+  }
+    data:any;
+   api: string= 'https://fakestoreapi.com/products';
+  // fetechData(api:string){
+  //   fetch(api)
+  //           .then((response)=>{
+  //             console.log(response) 
+  //             return response.json()  
+  //         })
+  //        .then ((data)=>{this.data=data;})
+          
+  //        .catch((err=>alert(err)))
+  //       }
+        async fetchData(api: string){
+          try{
+          const response=await fetch(api)
+          if(response.ok){
+          
+          const data=await response.json()
+          this.data=data;
+            }
+          else{
+            alert("error")
+          }
+          
+        }
+        catch(err){
+          alert(err)
+        }
+           
+        
+        }
+     }
